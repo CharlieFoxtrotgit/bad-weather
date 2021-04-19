0908ef75f6ecea5aabdf128ec1543948fe6d6df0
Charlie Hordern <acwhordern@gmail.com>
2021-04-19 10:55:42 +0100

    Logout link works and redirects to index

--- bad_weather/urls.py
+++ bad_weather/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("register", views.register, name="register"),
-    path('accounts/login', auth_views.LoginView.as_view(template_name='bad_weather/login.html'), name="login")
+    path('accounts/login', auth_views.LoginView.as_view(template_name='bad_weather/login.html'), name="login"),
+    path('accounts/logout', auth_views.LogoutView.as_view(next_page="index"), name="logout")
 ]
